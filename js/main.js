7c48c9e9c1b241ecb4ac51a8eac9ad38c9d47567
El-Ayvar <=>
2025-03-04 11:08:07 -0600

    primer ejercicio de arrayy

---
+++ js/main.js
@@ -0,0 +1,4 @@
+let arrayFrase = ['no', 'he', 'fracasado,', 'simplemente', 'me', 'he', 'topado', 'con', 'diez', 'mil', 'soluciones', 'equivocadas']
+console.log(arrayFrase)
+let separadoEspacio = arrayFrase.join(' ')
+console.log(separadoEspacio)
